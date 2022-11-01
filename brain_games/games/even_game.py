a034f9c0b81df302b64ab34c8f0e75bb44e5adab
KarinA <abdullaeva.karina.r@gmail.com>
2022-11-01 22:50:35 +0300

    refactor even_game.py

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -4,16 +4,12 @@ import random
 RULE = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def get_answer(number):
-    even_num = 'yes'
-    odd_num = 'no'
-    if number % 2 == 0:
-        return even_num
-    else:
-        return odd_num
-
-
 def get_content():
     question = random.randint(0, 100)
-    cor_answer = get_answer(question)
-    return [question, cor_answer]
+
+    if question % 2 == 0:
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
+
+    return [question, correct_answer]
