428d068c4db900ccf68550470a65c445389bf9d3
KarinA <abdullaeva.karina.r@gmail.com>
2022-11-02 12:06:07 +0300

    refactor the cognitivity complexity in prime_game.py

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -5,15 +5,13 @@ RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number):
-    """The first prime number is 2"""
-    if number > 1:
-        divisors = range(2, number + 1)
-        for element in divisors:
-            if number % element == 0 and element == number:
-                return 'yes'
-            elif number % element == 0 and element != number:
-                return 'no'
-            continue
+    divisors = range(2, number + 1)
+    for element in divisors:
+        if number % element == 0 and element == number:
+            return 'yes'
+        elif number % element == 0 and element != number:
+            return 'no'
+        continue
     return 'no'
 
 
