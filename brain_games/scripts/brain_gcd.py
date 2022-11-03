625ffa62ebf8c744885da01732d552ff05aed355
KarinA <abdullaeva.karina.r@gmail.com>
2022-11-03 16:12:50 +0300

    rename gcd_game.py in gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -3,12 +3,12 @@
 """Brain Greatest Common Divisor (GCD) Game"""
 
 from brain_games.general_logic import play
-from brain_games.games import gcd_game
+from brain_games.games import gcd
 
 
 def main():
     """Start to play GCD Game"""
-    play(gcd_game)
+    play(gcd)
 
 
 if __name__ == '__main__':
