3b76e7bb4bea73a345e5f79a9dfc504d9b897366
KarinA <abdullaeva.karina.r@gmail.com>
2022-11-04 00:27:55 +0300

    refactor prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,19 +5,20 @@ RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number):
-    divisors = range(2, int(math.sqrt(number) + 1))
-    for element in divisors:
-        if number % element == 0:
-            return False
-        elif number % element != 0:
-            continue
-    return True
+    if number > 1:
+        divisors = range(2, int(math.sqrt(number) + 1))
+        for element in divisors:
+            if number % element == 0:
+                return False
+            elif number % element != 0:
+                continue
+        return True
 
 
 def get_content():
     question = random.randint(0, 50)
 
-    if question > 1 and is_prime(question):
+    if is_prime(question):
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
